6e5a04607eac83e8b67ee781fe09eda4e12afd54
vadsavin <vadsavin@mail.ru>
2021-11-03 16:48:57 +0300

    fixes.

--- SafeBoard_ScanService/Server/ScanServer.cs
+++ SafeBoard_ScanService/Server/ScanServer.cs
@@ -1,9 +1,5 @@
 ﻿using NetCoreServer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SafeBoard_ScanAPI.Server
 {
--- SafeBoard_ScanService/utils/DefaultMessages.cs
+++ SafeBoard_ScanService/utils/DefaultMessages.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SafeBoard_ScanService.Utils
+﻿namespace SafeBoard_ScanService.Utils
 {
     public class DefaultMessages
     {
